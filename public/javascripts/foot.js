7c674ace6d9c1adbde7e464c69acc121fa9a8eee
David Morton <david@davids-macbook.attlocal.net>
2016-04-07 22:12:29 -0500

    Minor changes to the resume for better mobile viewing.

--- public/javascripts/foot.js
+++ public/javascripts/foot.js
@@ -1,9 +1,3 @@
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-}
+$(document).ready(function() { 
+	$('#profile').collapse('toggle');
+})
