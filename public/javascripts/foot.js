2edca67c12b343a223d4daf42ae1a78749fac050
David Morton <david@davids-macbook.attlocal.net>
2016-04-18 09:02:35 -0500

    Update to the scrolling site

--- public/javascripts/foot.js
+++ public/javascripts/foot.js
@@ -1,36 +1,77 @@
-$(document).ready(function() { 
-	$('#profile, #skills, #exp').collapse('toggle');
+function ScrollToElement(jqElement) {
+	var currentPosition = jqElement.offset().top;
 
-	$('.sectionContent, .experienceDetails').on('show.bs.collapse', function (e) {
+	var h1Height = $('h1').outerHeight();
+	
+	var scrollElement = null;
+
+	if ($(window).width() >= 700) {
+		var scrollElement = $('.innerWrapper');
+
+		var currentScroll = scrollElement.scrollTop();
+
+		var result = (currentScroll + currentPosition) - h1Height;
+	} else {
+		var scrollElement = $('body');
+
+		var result = currentPosition - h1Height;
+	}
+
+	scrollElement.animate({
+    	scrollTop: result
+	}, 200);
+}
+
+function OnCollapse(e) {
+	e.stopPropagation();
+
+	var collapsedTarget = $(e.currentTarget).closest('.experience, .section');
+
+	if (collapsedTarget.hasClass('section')) {
+		ScrollToElement($('.body-content'));
+	} else {
+		ScrollToElement($('#exp').closest('.section'));
+	}
+}
+
+function OnShow(e) {
+	e.stopPropagation();
+
+	var elementToScrollTo = $(e.currentTarget).closest('.experience, .section');
+
+	$('.sectionContent, .experienceDetails').off('hide.bs.collapse');
+
+	if (elementToScrollTo.hasClass('section')) {
+		$('.section .sectionContent').not($(e.currentTarget)).collapse('hide');
+	} else {
+		$('.experience .experienceDetails').not($(e.currentTarget)).collapse('hide');
+	}
+
+	$('.sectionContent, .experienceDetails').on('hide.bs.collapse', OnCollapse);
+
+
+	$(e.currentTarget).on('shown.bs.collapse', function(e) {
 		e.stopPropagation();
 
-		if ($(window).width() >= 700) {
-			var scrollElement = $('.wrapper');
-
-			var h1Height = $('h1').outerHeight();
-			var currentPosition = $(e.currentTarget).closest('.experience, .section').offset().top;
-			var currentScroll = scrollElement.scrollTop();
-			var result = (currentScroll + currentPosition) - h1Height;
-
-			console.log('h1Height: ' + h1Height + ' currentPosition: ' + currentPosition + ' currentScroll: ' + currentScroll + ' result: ' + result);
-
-			scrollElement.animate({
-		    	scrollTop: result
-			}, 200);
-		} else {
-			var scrollElement = $(window);
-
-			var h1Height = $('h1').outerHeight();
-			var currentPosition = $(e.currentTarget).closest('.experience, .section').offset().top;
-			var result = currentPosition - h1Height;
-
-			console.log('h1Height: ' + h1Height + ' currentPosition: ' + currentPosition + ' currentScroll: ' + currentScroll + ' result: ' + result);
-
-			//scrollElement.scrollTop(result);
-			$('body').animate({
-		    	scrollTop: result
-			}, 200);
-		}
+		ScrollToElement($(e.currentTarget).closest('.experience, .section'));
 	});
+}
+
+function OnShown(e) {
+	e.stopPropagation();
+
+	$('#profile').off('shown.bs.collapse');
+
+	$('.sectionContent, .experienceDetails').on('show.bs.collapse', OnShow);
+}
+
+$(document).ready(function() { 
+	ScrollToElement($('.body-content'));
+
+	$('#profile').collapse('toggle');
+
+	$('#profile').on('shown.bs.collapse', OnShown);
+
+	$('.sectionContent, .experienceDetails').on('hide.bs.collapse', OnCollapse);
 });
 
