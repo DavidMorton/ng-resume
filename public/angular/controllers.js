f9ff4dee227efd65ef695b7484822c58c6ea6d5b
David Morton <david@davids-macbook.attlocal.net>
2016-05-04 16:43:43 -0500

    Figured out how to prevent the reload. Now I just need to have the original page scroll to the right place.

--- public/angular/controllers.js
+++ public/angular/controllers.js
@@ -1,4 +1,5 @@
-resumeApp.controller('resumeController', ['$scope', '$timeout', '$http', '$routeParams', function($scope, $timeout, $http, $routeParams) {
+resumeApp.controller('resumeController', ['$scope', '$timeout', '$http', '$routeParams', '$route', 
+    function($scope, $timeout, $http, $routeParams, $route) {
     $http.get('/json/resume.json')
     .then(function(res) {
         $scope.data = res.data;
@@ -8,6 +9,11 @@ resumeApp.controller('resumeController', ['$scope', '$timeout', '$http', '$route
         ScrollToElement($('.body-content'));
 
         $('#profile').collapse('show');
+
+        var lastRoute = $route.current;
+        $scope.$on('$locationChangeSuccess', function(event) {
+            $route.current = lastRoute;
+        });
 
         $timeout(function() { 
             $('.sectionContent, .experienceDetails').on('show.bs.collapse', OnShow);
